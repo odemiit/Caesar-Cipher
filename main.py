d82436b91782d84b75e7290824250e0f6770e1cb
Odemi Tosanwumi <46735063+odemiit@users.noreply.github.com>
2024-07-01 23:06:59 +0000

    first attempt at shifting the text based on the user's input

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 def encrypt(text, shift):
-    
-
     #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
     #e.g. 
     #plain_text = "hello"
     #shift = 5
     #cipher_text = "mjqqt"
     #print output: "The encoded text is mjqqt"
+    encrypted_text = ""
+    for letter in text:
+        original_index = alphabet.index(letter)
+        encrypted_text += alphabet[original_index + 5]
 
     ##HINT: How do you get the index of an item in a list:
     #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
