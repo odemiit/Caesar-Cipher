7aa8b218a4f63595eddb69f807806cb76a32d94e
Odemi Tosanwumi <46735063+odemiit@users.noreply.github.com>
2024-07-02 00:49:55 +0000

    Part 4 start instructions (improving the user experience)

--- main.py
+++ main.py
@@ -11,39 +11,25 @@ def caesar(sample_text, shift_amount, direction):
           shift_amount *= -1
     
     for letter in sample_text:
+        #TODO-3: What happens if the user enters a number/symbol/space?
+        #Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
+        #e.g. start_text = "meet me at 3"
+        #end_text = "•••• •• •• 3"
         original_index = alphabet.index(letter)
         final_text += alphabet[original_index + shift_amount]
         
-    print(f"The {direction}d text is {final_text}")
-    
+    print(f"Here's the {cipher_direction}d result: {end_text}")
 
-# def encrypt(plain_text, shift_amount):
-#     encrypted_text = ""
-#     for letter in plain_text:
-#         original_index = alphabet.index(letter)
-#         if original_index + shift_amount < len(alphabet):
-#             encrypted_text += alphabet[original_index + shift_amount]
-#         else:
-#             encrypted_text += alphabet[(original_index + shift_amount) - len(alphabet)]
+#TODO-1: Import and print the logo from art.py when the program starts.
 
-#     print(f"The encoded text is {encrypted_text}")
+#TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
+#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
+#If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
+#Hint: Try creating a while loop that continues to execute the program if the user types 'yes'. 
 
-# def decrypt(cipher_text, shift_amount):
-#     decrypted_text = ""
-#     for letter in cipher_text:
-#         original_index = alphabet.index(letter)
-#         if original_index - shift_amount >= 0:
-#             decrypted_text += alphabet[original_index - shift_amount]
-#         else:
-#             decrypted_text += alphabet[(original_index - shift_amount) + len(alphabet)]
+#TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
+#Try running the program and entering a shift number of 45.
+#Add some code so that the program continues to work even if the user enters a shift number greater than 26. 
+#Hint: Think about how you can use the modulus (%).
 
-#     print(f"The decoded text is {decrypted_text}")
-
-#Call the correct function based on what "direction" the user inputs
-# if direction == "encode":
-#     encrypt(text, shift)
-# elif direction == "decode":
-#     decrypt(text, shift)
-
-#TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
 caesar(text, shift, direction)
