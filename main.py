44a5ca0c13b45ac83221d1ad40bb8730a5284e97
Odemi Tosanwumi <46735063+odemiit@users.noreply.github.com>
2024-07-02 00:10:03 +0000

    check if the user wants to encrypt or decrypt based on the "direction" inputed and call the right function

--- main.py
+++ main.py
@@ -26,15 +26,11 @@ def decrypt(plain_text, shift_amount):
             decrypted_text += alphabet[(original_index - shift_amount) + len(alphabet)]
 
     print(f"The decoded text is {decrypted_text}")
-    
-  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
-  #e.g. 
-  #cipher_text = "mjqqt"
-  #shift = 5
-  #plain_text = "hello"
-  #print output: "The decoded text is hello"
 
 
 #TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
-encrypt(text, shift)
-decrypt(text, shift)
+#encrypt(text, shift)
+if direction == "encode":
+    encrypt(text, shift)
+elif direction == "decode":
+    decrypt(text, shift)
