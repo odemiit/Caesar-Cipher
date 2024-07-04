57a8d7b43566c8ce86eb099fa47cb8112ce854fc
Odemi Tosanwumi <46735063+odemiit@users.noreply.github.com>
2024-07-04 16:45:54 +0000

    import and printing the logo of the game

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
+from art import logo
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
 def caesar(sample_text, shift_amount, direction):
     final_text = ""
     if direction == "decode":
@@ -21,11 +18,16 @@ def caesar(sample_text, shift_amount, direction):
     print(f"Here's the {cipher_direction}d result: {end_text}")
 
 #TODO-1: Import and print the logo from art.py when the program starts.
+print(logo)
 
 #TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
 #e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
 #If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
 #Hint: Try creating a while loop that continues to execute the program if the user types 'yes'. 
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
 
 #TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
 #Try running the program and entering a shift number of 45.
