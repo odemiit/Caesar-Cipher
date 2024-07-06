69ec45abfec1c24838cc6dcf6e2504f11f6b1b67
Odemi Tosanwumi <46735063+odemiit@users.noreply.github.com>
2024-07-06 19:22:08 +0000

    Final solution with clean code

--- main.py
+++ main.py
@@ -8,10 +8,6 @@ def caesar(sample_text, shift_amount, direction):
           shift_amount *= -1
     
     for letter in sample_text:
-        #TODO-3: What happens if the user enters a number/symbol/space?
-        #Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
-        #e.g. start_text = "meet me at 3"
-        #end_text = "•••• •• •• 3"
         if letter not in alphabet:
             final_text += letter
         else:
@@ -25,13 +21,8 @@ def caesar(sample_text, shift_amount, direction):
         
     print(f"Here's the {direction}d result: {final_text}")
 
-#TODO-1: Import and print the logo from art.py when the program starts.
+#print the logo from art.py when the program starts.
 print(logo)
-
-#TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
-#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
-#If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
-#Hint: Try creating a while loop that continues to execute the program if the user types 'yes'. 
 
 restart = True
 
@@ -49,9 +40,4 @@ while restart is True:
     
     if restart_answer == "no":
         restart = False
-
-#TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
-#Try running the program and entering a shift number of 45.
-#Add some code so that the program continues to work even if the user enters a shift number greater than 26. 
-#Hint: Think about how you can use the modulus (%).
 
